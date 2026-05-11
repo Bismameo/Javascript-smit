55ffe9a4f336592aac556006e04db9248391551d
yaseen <bisma202>
2026-05-12 00:06:04 +0500

    update

---
+++ promise.js
@@ -0,0 +1,22 @@
+let check = new Promise((resolve, reject) => {
+    let checkValue = false
+    if (checkValue) {
+        resolve ("yes fullfilled")
+    }
+    else {
+        reject("rejected")
+    }
+})
+
+check 
+ .then((message) => console.log(message))
+ .catch((error) => console.error(error));
+
+
+ let password = new Promise ((login, rejected)){
+    let value = true
+ }
+
+
+
+ 
